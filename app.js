b8dd1b1289a30d27c8ef8d1d732cf82fb036afd7
Kuba Biel <jakubiel@o2.pl>
2022-01-30 14:31:26 +0100

    s

--- app.js
+++ app.js
@@ -1,25 +1,38 @@
 #!/usr/bin/env node
 
 const fs = require("fs");
+const { lstat } = fs.promises;
 
-fs.readdir(process.cwd(), (err, filenames) => {
+fs.readdir(process.cwd(), async (err, filenames) => {
 	if (err) {
 		//if err is defined
 		console.log(err);
 	}
-	console.log(filenames);
+	//create an object with all callbacks
+	const statPromises = filenames.map((filename) => {
+		return lstat(filename);
+	});
+
+	const allStats = await Promise.all(statPromises);
+
+	for (let stats of allStats) {
+		const index = allStats.indexOf(stats);
+
+		console.log(filenames[index], stats.isFile());
+	}
 });
 
-// const lstat = filename =>{
-// 	return new Promise ((resolve, reject)=>{
-// 		fs.lstat(filename, (err, stats)=>{
-// 			if(err){
-// 				reject (err)
+//WRAPPING lstat with promise:
+// const lstat = (filename) => {
+// 	return new Promise((resolve, reject) => {
+// 		fs.lstat(filename, (err, stats) => {
+// 			if (err) {
+// 				reject(err);
 // 			}
-// 			resolve(stats)
-// 		})
-// 	})
-// }
-
-// const util = require('util')
-// const lstat = util.promisify(fs.lstat)
+// 			resolve(stats);
+// 		});
+// 	});
+// };
+//				OR
+// const util = require("util");
+// const lstat = util.promisify(fs.lstat);
